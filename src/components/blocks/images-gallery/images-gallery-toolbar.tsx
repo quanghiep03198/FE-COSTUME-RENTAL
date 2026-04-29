054c1b153cce5214dc55ad37799a6ddd4c6a5c79
quanghiep03198 <quanghiep03198@gmail.com>
2026-04-29 16:34:08 +0700

    update

---
+++ src/components/blocks/images-gallery/images-gallery-toolbar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const ImagesGalleryToolbar :React.FC= () => {
+  return (
+    <div>ImagesGalleryToolbar</div>
+  )
+}
+
+export default ImagesGalleryToolbar
