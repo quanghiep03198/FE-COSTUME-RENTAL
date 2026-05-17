2bcc899a0cc32c163246dfec1f4341828c79b749
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-17 22:26:12 +0700

    refactor: update getImageUrl function for improved path handling and environment variable usage

--- src/common/helpers/get-image-url.ts
+++ src/common/helpers/get-image-url.ts
@@ -1,3 +1,7 @@
-export const getImageUrl = (path: string) => {
-  return `${import.meta.env.VITE_BASE_IMAGE_URL}${path}`
+import { path } from '@/lib/polyfill'
+import env from '@/lib/utils'
+
+export const getImageUrl = (url: string) => {
+  url = path.join('/storage/images-gallery', url) // Ensure the path starts with a slash and is correctly joined
+  return new URL(url, env('VITE_EXTERNAL_API_URL')).toString()
 }
