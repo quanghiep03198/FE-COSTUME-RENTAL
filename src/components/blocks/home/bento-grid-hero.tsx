b80726350c9e6472b2b877d6514df6d24a3207e4
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-19 00:15:23 +0700

    fix(bento-grid-hero): adjust image heights and remove duplicate image

--- src/components/blocks/home/bento-grid-hero.tsx
+++ src/components/blocks/home/bento-grid-hero.tsx
@@ -7,7 +7,7 @@ import type React from 'react'
 const BentoGridHero: React.FC = () => {
   return (
     <section className="xl:container xxl:p-0 px-4 mx-auto grid xl:grid-cols-3 gap-6 md:max-xl:grid-cols-2 grid-cols-1 grid-rows-2 [&_*[data-slot=card-title]]:text-white [&_*[data-slot=card-description]]:text-white">
-      <Card className="col-start-1 xl:row-span-full p-0 relative lg:min-h-135 min-h-100">
+      <Card className="col-start-1 xl:row-span-full p-0 relative lg:min-h-160 min-h-100">
         <img src="/ao-dai-thu-bao.jpg" className="w-full object-cover absolute inset-0 object-top z-0" />
         <CardContent className="flex flex-col items-start gap-y-1.5 relative z-10 mt-auto py-6 bg-linear-to-t from-[#00ad9695] to-75% to-transparent rounded-none">
           <CardTitle className="text-xl font-semibold">Áo dài Nữ</CardTitle>
@@ -21,6 +21,7 @@ const BentoGridHero: React.FC = () => {
         </CardContent>
       </Card>
       <Card className="xl:col-start-2 relative xl:row-span-full p-0 bg-primary/10 text-primary gap-0">
+        <img src="/ao-dai-gia-hy.jpg" className="w-full absolute inset-0 z-0 object-contain object-bottom " />
         <CardContent className="flex flex-col items-start gap-y-1.5 z-10 xl:bg-linear-to-b bg-linear-to-t mt-auto xl:mt-0 from-[#f4e8e3] to-50% to-transparent py-6 ">
           <CardTitle className="text-xl font-semibold">Áo dài Nam</CardTitle>
           <CardDescription className="mb-3">
@@ -30,7 +31,6 @@ const BentoGridHero: React.FC = () => {
             Xem thêm <ArrowRightIcon className="size-3 transition-transform duration-200 group-hover:translate-x-0.5" />
           </Link>
         </CardContent>
-        <img src="/ao-dai-gia-hy.jpg" className="w-full absolute inset-0 z-0 object-contain object-bottom " />
       </Card>
       <Card className="xl:col-start-3 sm:max-lg:col-span-full relative row-span-1 p-0 bg-primary/10 text-primary gap-0">
         <CardContent className="flex flex-col items-start gap-y-1.5 relative z-10 xl:bg-linear-to-b bg-linear-to-t mt-auto xl:mt-0 from-[#ef756695] to-75% to-transparent py-6 ">
