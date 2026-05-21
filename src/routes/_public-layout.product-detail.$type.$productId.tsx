3b88bf832d8288eb3b95040d61c88135ef8206e1
quanghiep03198 <quanghiep03198@gmail.com>
2026-05-21 16:35:32 +0700

    feat: refactor product detail loader to include category detail retrieval

--- src/routes/_public-layout.product-detail.$type.$productId.tsx
+++ src/routes/_public-layout.product-detail.$type.$productId.tsx
@@ -1,3 +1,4 @@
+import { getOneCategoryQueryOptions } from '@/apis/category/hooks/use-category-request'
 import { getCostumeDetailQueryOptions } from '@/apis/costume/hooks/use-costume-request'
 import { getPropsDetailQueryOptions } from '@/apis/equipment-props/hooks/use-equipment-props-request'
 import ProductDetailPage from '@/components/blocks/product-detail'
@@ -17,8 +18,26 @@ export const Route = createFileRoute('/_public-layout/product-detail/$type/$prod
     },
   },
   loader: async ({ context, params: { type, productId } }) => {
-    if (type === 'costume') return await context.queryClient.ensureQueryData(getCostumeDetailQueryOptions(productId))
-    if (type === 'equipment-props')
-      return await context.queryClient.ensureQueryData(getPropsDetailQueryOptions(productId))
+    let productDetail,
+      productCategoryDetail = null
+
+    let joinCollection: Nullable<'costumes' | 'equipment_props'> = null
+
+    if (type === 'costume') {
+      joinCollection = 'costumes'
+      productDetail = await context.queryClient.ensureQueryData(getCostumeDetailQueryOptions(productId))
+    }
+
+    if (type === 'equipment-props') {
+      joinCollection = 'equipment_props'
+      productDetail = await context.queryClient.ensureQueryData(getPropsDetailQueryOptions(productId))
+    }
+
+    if (productDetail && joinCollection)
+      productCategoryDetail = await context.queryClient.ensureQueryData(
+        getOneCategoryQueryOptions(productDetail.category.id, joinCollection)
+      )
+
+    return { productDetail, productCategoryDetail }
   },
 })
